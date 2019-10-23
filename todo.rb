e6f67e58c154a62e10a0934b4c859d020b8e9a20
tprax <tprax01@gmail.com>
2019-10-23 13:59:41 -0600

    Testing things out

--- todo.rb
+++ todo.rb
@@ -6,3 +6,12 @@ complete a task
 uncomplete a task
 /
 puts "hello world"
+puts "How are you doing today?"
+answer = gets.to_s
+puts answer
+
+if answer === "good"
+  puts "That is great to hear!"
+else
+  puts "that is not good to hear."
+end
