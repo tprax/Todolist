b87f2c7b1f6f92d2799981e6d0ca407372907580
tprax <tprax01@gmail.com>
2019-10-27 20:08:12 -0600

    new program

---
+++ todopart2.rb
@@ -0,0 +1,24 @@
+/
+Notes
+This is a second part to this todo list for more on learning or note
+taking for ruby programming langauge
+/
+
+/First create and add variables/
+firstname = 'Tyler'
+lastname = 'Prax'
+world = 'Hello World'
+number = 2
+float = 2.0
+boolean = true
+/
+puts firstname
+puts lastname
+puts world
+puts number
+puts float
+puts boolean
+/
+/Notes to on outputs and inputs from the user/
+puts "What is your first name"
+firstname = gets.to_s
