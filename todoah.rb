b1bdf56203bf1551e23d41344b6adea95944cade
tprax <tprax01@gmail.com>
2019-11-03 12:39:55 -0700

    add notes

--- todoah.rb
+++ todoah.rb
@@ -1,6 +1,8 @@
 /arrays and hashes notes/
-
+/array is a collection of data itmes that can be selected by indices computed
+at run-time/
 arr = Array.new(0)
 arr = Array.new(5)
 arr = []
+arr = ["orange", "apple", "pear"]
 puts arr
