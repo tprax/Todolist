08edb0afc3f714b216a807b5d8379b979232c46d
tprax <tprax01@gmail.com>
2019-11-03 12:43:03 -0700

    notes

--- todoah.rb
+++ todoah.rb
@@ -1,6 +1,8 @@
 /arrays and hashes notes/
 /array is a collection of data itmes that can be selected by indices computed
 at run-time/
+/arrays are zero based/
+
 arr = Array.new(0)
 arr = Array.new(5)
 arr = []
