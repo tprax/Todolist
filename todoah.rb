bf94e47c515e611e11ee5515c5e24cf98bd77c15
tprax <tprax01@gmail.com>
2019-11-04 15:50:21 -0700

    update

--- todoah.rb
+++ todoah.rb
@@ -21,36 +21,29 @@ end
 end
 
 /modifying arrays/
-/test_arr = [ "A", "B", "C" ]
+test_arr = [ "A", "B", "C" ]
 test_arr << "D"
-puts test_arr
 
 tes_arr = ["B", "C", "D"].unshift("A")
-puts test_arr
 
 test_arr.shift
-puts test_arr
 
 test_arr.pop
-puts test_arr
 
 test_arr.insert(1, "z")
-puts test_arr
 
 arr_test2 = [1,2,3].rotate
-puts arr_test2
 
 arr_test3 = [1,2,3].reverse
-puts arr_test3
 
 arr_sort = ["S","Z","P","T"].sort
-puts arr_sort
+puts arr_sort.join
 
 arr.delete_at(1)
 puts arr
 
 arr.clear
-puts arr/
+puts arr
 
 /hashes notes/
 /A hash is a dictionary-like collection of unique keys and their values. Also called associative arrays,
