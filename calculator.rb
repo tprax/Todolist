3f24245e38e19be1c00acf07aacf52a2766b9e29
tprax <tprax01@gmail.com>
2019-11-08 21:40:05 -0700

    new file to program

---
+++ calculator.rb
@@ -0,0 +1,22 @@
+/Sudo Ruby Calculator/
+/get first number
+get modifer
+get second number
+puts results/
+/add error checking/
+
+def first_number
+
+end
+
+def modifer
+
+end
+
+def second_number
+
+end
+
+def results
+
+end
